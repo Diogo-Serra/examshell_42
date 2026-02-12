82c65648541af00088d6a2e1554b34a5c10c597a
Diogo-Serra <di-code@outlook.com>
2026-02-12 21:40:38 +0000

    updates

--- rendu/rev_wstr/rev_wstr.c
+++ rendu/rev_wstr/rev_wstr.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 21:03:48 by diosoare          #+#    #+#             */
-/*   Updated: 2026/02/12 21:36:29 by diosoare         ###   ########.fr       */
+/*   Updated: 2026/02/12 21:40:20 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,29 @@ int	main(int argc, char **argv)
 	int start;
 	int	end;
 
-	i = 0;
 	if (argc == 2)
 	{
+		i = 0;
 		while (argv[1][i])
+			i++;
+		while (i >= 0)
 		{
-			while (argv[1][i] == ' ')
-				i++;
-			start = i;
-			while (argv[1][i] && argv[1][i] != ' ')
-				i++;
-			end = i - 1;
+			while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\t'))
+				i--;
+			end = i;
+			while (i >= 0 && argv[1][i] != ' ' && argv[1][i] != '\t')
+				i--;
+			start = i + 1;
 			if (start <= end)
 			{
-				int j = end;
-				while (j >= start)
-					write(1, &argv[1][j--], 1);
-				if (argv[1][i])
+				int j = start;
+				while (j <= end)
+					write(1, &argv[1][j++], 1);
+				if (i >= 0)
 					write(1, " ", 1);
 			}
 		}
 	}
-	else
-		write(1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
